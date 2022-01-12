7a76e078b19c0eeae79b4bf2ec915a4b44fc190a
Frederic Gernez <frederic.gernez@fr.ibm.com>
2022-01-12 14:13:43 +0100

    new 3

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!" + os.getenv("fred")
 
 if __name__ == "__main__":
     application.run()
